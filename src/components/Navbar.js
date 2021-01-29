14f0b910652d3902704a49fbda34cde8f3450084
Jun Inoue <gaspardcube15@gmail.com>
2021-01-30 02:17:34 +0900

    add plugin for scss

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import dumplingLogo from "../assets/dumpling-logo.svg"
+import soupDumplingLogo from "../assets/soup-dumpling-logo.svg"
+import { FaAlignRight } from "react-icons/fa"
+import PageLinks from "../constants/links"
+const Navbar = () => {
+  return <></>
+}
+
+export default Navbar
