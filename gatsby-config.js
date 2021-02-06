99a6e8d4cb123903a1ea2220ee93ec6b998e870b
Jun Inoue <gaspardcube15@gmail.com>
2021-02-06 14:58:11 +0900

    add about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,11 +31,9 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
-        //   contentTypes : `jobs`, `projects`, `blogs`,
-        //   singleType : `about`
+        singleTypes: [`about`],
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
         contentTypes: [`jobs`, `works`, `blogs`],
-        singleTypes: [],
       },
     },
     {
